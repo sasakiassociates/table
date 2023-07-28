72037a8310a93e265d9ce94f101a627d38f6cc19
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2023-07-28 11:25:44 -0400

    skeleton

---
+++ FirebaseSharp/FirebaseSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Firebase;
+using System;
+
+
+Console.WriteLine("starting application");
+
+/* create a connection with firebase */
+/* setup new markers */
+/* get the info for markers */
+/* read in the openCV data */
---
+++ FirebaseSharp/TableUi/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TableUi
+{
+
+  class Program
+  {
+    
+    static void Main(string[] args)
+    {
+      
+      // direct connection to openCV
+      
+      Console.WriteLine("Hello World!");
+    }
+  }
+
+}
