6e1b5d379a896c85554face529a174d06bc6f1a3
KaipoCraft <71410871+KaipoCraft@users.noreply.github.com>
2023-10-27 14:29:29 -0400

    creation of zone markers

--- src/detector/markerFactory.py
+++ src/detector/markerFactory.py
@@ -1,47 +1,72 @@
 import json
 from . import marker as m
 # import marker as m
+from . import zone as z
 
 import os
 import glob
 
+# Static variables
+CAMERA_MARKER_ID = 0
+NUM_OF_ZONES = 1
+NUM_MARKERS_PER_ZONE = 3
+
 class MarkerFactory:
     @staticmethod
-    def make_markers(dict_length, observer, timer_):
+    def make_markers(dict_length, repository_, timer_):
         marker_list = []
 
         # First, let's make the project marker set (making sure they are not the same as the controller marker ids)
         # json_files = MarkerFactory.load_json_files("..\\projects")  # Find all files in the project folder
         json_files = MarkerFactory.load_json_files("..\\projects")  # Find all files in the project folder
 
-        project_marker_ids = []
+        assigned_marker_ids = []
 
         if len(json_files) > 0:
             for file in json_files:
                 marker_id = int(file['marker_id'])
-                if marker_id > 1 & marker_id < dict_length:
-                    project_marker_ids.append(marker_id)           # Build a list of all the marker ids associated with a project file
-                elif marker_id == 0:
+                if marker_id == CAMERA_MARKER_ID:
                     print(f"ERROR: {file['name']} has an ID ({marker_id}) that is reserved for camera control. Please change the ID of this project.")
+                elif marker_id >= 0 & marker_id < dict_length:
+                    id_ = int(file['marker_id'])
+                    new_project_marker = m.ProjectMarker(id_, timer_)
+                    new_project_marker.attach_observer(repository_)
+                    new_project_marker.associate_marker_with_project(file)
+                    marker_list.append(new_project_marker)
+
+                    assigned_marker_ids.append(marker_id)                       # Add to the list of already assigned marker ids
                 else:
                     print(f"ERROR: {file['name']} has an ID ({marker_id}) that is out of range for this dictionary. The highest id in this dictionary is {dict_length - 1}. Please change the ID of this project.")
-            for marker_id in project_marker_ids:
-                marker_id = int(marker_id)
-                new_project_marker = m.ProjectMarker(marker_id, timer_)
-                new_project_marker.attach_observer(observer)
-                for file in json_files:                                     # If a marker id matches the associated marker id of a project file, associate the project file with the marker
-                    if int(file['marker_id']) == marker_id:
-                        new_project_marker.associate_marker_with_project(file)
-                        break
-                marker_list.append(new_project_marker)              # Make a project marker for each marker id associated with a project file
+
+        # Now let's make the zones
+        # TODO change from the default of 10 zones to a user defined number
+        for i in range(0, NUM_OF_ZONES):
+            end_of_dict = dict_length
+            # Check to make sure it hasn't already been assigned
+            # Make the zone
+            zone_ = z.Zone()
+
+            marker_id_start = end_of_dict - ((i+1) * NUM_MARKERS_PER_ZONE)
+            # Make the markers that will be in the zone
+            for j in range(0, NUM_MARKERS_PER_ZONE):
+                id_ = marker_id_start + j
+                if id_ in assigned_marker_ids:
+                    # This assumes it's a project, maybe we should check the type of marker first
+                    conflicting_project = marker_list[assigned_marker_ids.index(i)].project_name
+                    print(f"ERROR: Zone {i} has already been assigned to a project. Please change the ID of project: {conflicting_project}.")
+                marker_ = m.GenericMarker(id_, timer_)
+                marker_.attach_observer(zone_)
+                marker_list.append(marker_)
+                print(f"Created marker {id_} in zone {i}")
+                assigned_marker_ids.append(id_)
 
         # Finally, let's make the rest of the markers generic markers
         for i in range(0, dict_length):
-            if i not in project_marker_ids:
+            if i not in assigned_marker_ids:
                 marker_list.append(m.GenericMarker(i, timer_))
-                marker_list[i].attach_observer(observer)
+                marker_list[i].attach_observer(repository_)
         
-        marker_list[0].type = "camera"
+        marker_list[CAMERA_MARKER_ID].type = "camera"
 
         print(f"Created {len(marker_list)} markers")
 
