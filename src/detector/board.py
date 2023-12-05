a779e10d26d201bbe24040788d73dca4c11746f9
KaipoCraft <71410871+KaipoCraft@users.noreply.github.com>
2023-12-05 16:00:28 -0500

    detection of multiple markers working

--- src/detector/board.py
+++ src/detector/board.py
@@ -1,11 +1,23 @@
 import cv2 as cv
 import numpy as np
+import threading
 
 from . import marker as m
 from . import timer as t
 from . import state as s
 
-class Board():
+class BoardSingletonMeta(type):
+    _instances = {}
+    _lock = threading.Lock()
+
+    def __call__(cls, *args, **kwargs):
+        with cls._lock:
+            if cls not in cls._instances:
+                instance = super().__call__(*args, **kwargs)
+                cls._instances[cls] = instance
+            return cls._instances[cls]
+
+class Board(metaclass=BoardSingletonMeta):
     def __init__(self, repository) -> None:
         self.markers = {}       # The markers currently present on the board with their ids as the keys
         self.bounds = None      # Holds the (x, y) coordinates of the calibration markers from setup
@@ -14,35 +26,41 @@ class Board():
         self.repository = repository
         self.state = None
         self.trigger_state_pairs = s.StateFactory.make_states(self)
+        self.markers_to_delete = []
 
-    def make_marker(self, id_, corners):
+    def make_marker(self, id_):
         new_marker = m.Marker(id_, self.timer)
         new_marker.attach_observer(self.repository)
-        new_marker.update(corners)
 
         # If the marker id is not in the dictionary, add it
         if id_ not in self.markers.keys():
             self.markers[id_] = []
         # Add the marker to the list of markers with that id
         self.markers[id_].append(new_marker)
+
+        return new_marker
     
-    def destroy_marker(self, uuid):
-        for id_, markers in self.markers.items():
-            for marker in markers:
-                if marker.uuid == uuid:
-                    markers.remove(marker)
-                    if len(markers) == 0:
-                        self.markers.pop(id_)
-                    return
+    def destroy_markers(self):
+        if len(self.markers_to_delete) > 0:
+            print(self.markers_to_delete)
+            print("Destroying markers")
+            for marker in self.markers_to_delete:
+                self.markers[marker.id].remove(marker)
+                if len(self.markers[marker.id]) == 0:
+                    del self.markers[marker.id]
+            self.markers_to_delete = []
 
     def get_marker(self, id_):
         for marker in self.markers[id_]:
             # If the marker was already updated, skip it
             if marker.updated:
                 continue
-            # Otherwise, update it and return it
-            else:
+            # If it hasn't been updated, update it and return it
+            elif not marker.updated:
                 return marker
+        # If all the markers have been updated, make a new one
+        marker = self.make_marker(id_)
+        return marker
 
     # Called by the Camera object
     # Runs through the detected ids and checks if they are already on the board
@@ -51,52 +69,58 @@ class Board():
     # If not, make a new marker object with a uuid and add it to the board
     # TODO expect there to be multiple markers with the same id
     def update(self, ids, corners):
-        # We want to go through all the markers on the board and see if they've been detected
-        # TODO have to find a way to check which ids/corners go with which marker objects
-
-        # check for multiple markers with the same id
-        # {id:
-        #   {marker,
-        #    marker},
-        #  id:
-        #   {marker}
-        # }
-
+        # Mark each marker as not updated
+        for id_, markers in self.markers.items():
+            for marker in markers:
+                marker.updated = False
+        
         if ids is not None:
-            # Mark each marker as not updated
-            for id_, markers in self.markers.items():
-                for marker in markers:
-                    marker.updated = False
+            processed_ids = set()
+
             for marker_id, marker_corners in zip(ids, corners):
                 id_ = int(marker_id)
-                if id_ in self.markers.keys():
-                    # 1. Marker exists and has been detected
-                    marker = self.get_marker(id_)
+                if id_ not in self.markers.keys():
+                    # 1. Marker does not exist and has been detected
+                    marker = self.make_marker(id_)
                     marker.update(marker_corners)
                 else:
-                    # 2. Marker does not exist and has been detected
-                    self.make_marker(id_, marker_corners)
-            # 3. Marker exists and has not been detected
-            for marker_id, marker in list(self.markers.items()):
-                if marker_id not in ids:
-                    if marker.is_visible:
-                        marker.lost_tracking()
-                    elif marker.gone:
-                        self.destroy_marker(marker.uuid)
-        else:
-            # 4. Marker does not exist and has not been detected
-            for id_, markers in self.markers.items():
-                for marker in markers:
-                    if marker.is_visible:
-                        marker.lost_tracking()
-                    elif marker.gone:
-                        self.destroy_marker(marker.uuid)
+                    # 2. Marker exists and has been detected
+                    # If the id does not exist in the processed_ids set, add it and update the marker
+                    if id_ not in processed_ids:
+                        marker = self.get_marker(id_)
+                        marker.update(marker_corners)
+                        processed_ids.add(id_)
+                    # If the id does exist in the processed_ids set, that means there are multiple markers in there (or should be)
+                    else:
+                        # Go through each of the markers with that id and check if they've been updated
+                        # If they have, skip them, and if they all have, make a new marker
+                        for marker in self.markers[id_]:
+                            if marker.updated:
+                                continue
+                            elif not marker.updated:
+                                marker.update(marker_corners)
+                                break
+                            else:
+                                marker = self.make_marker(id_)
+                                marker.update(marker_corners)
+                                break
         
+        # 3. Marker exists and has not been detected
+        for id_, markers in self.markers.items():
+            for marker in markers:
+                if marker.updated:
+                    continue
+                elif marker.gone:
+                    self.markers_to_delete.append(marker)
+                elif marker.is_visible:
+                    print("Lost tracking")
+                    marker.lost_tracking()
+        self.destroy_markers()
+
         if self.repository.new_data:
             self.repository.strategy.send()
             self.repository.new_data = False
 
-        # print(self.markers)
 
     def draw(self, frame):
         # Draw the markers
